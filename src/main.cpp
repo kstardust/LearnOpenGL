9247e7d2baa93951fd4418471f61d4c3e3a65a8c
liqilong <liqilong@corp.netease.com>
2021-10-26 19:13:27 +0800

    draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -51,12 +51,49 @@ int main()
     #endif
     );
 
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };  
+
+    unsigned int VBO, EBO;
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);    
+
+
     while (!glfwWindowShouldClose(window)) 
     {
         process_input(window);
-
+        
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);    
         glClear(GL_COLOR_BUFFER_BIT);
+
+        shader_prog.UseProgram();
+        glBindVertexArray(VAO);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
