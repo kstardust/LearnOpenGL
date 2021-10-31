d8f1bdcddc0994c4a5e80eccf4bc7c5c86017c50
stardust <lql9705@gmail.com>
2021-10-31 18:29:45 +0800

    texture

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,9 +1,14 @@
 #version 330 core
 out vec4 FragColor;
 in vec3 color;
+in vec2 texCoord;
+
+uniform sampler2D ufTexture;
+uniform sampler2D ufTexture2;
 
 uniform vec4 ufColor;
 void main()
 {
-    FragColor = vec4(color.xyz, 1.0f);
+  FragColor = mix(texture(ufTexture, texCoord), texture(ufTexture2, texCoord), 0.2);
+  // FragColor = texture(ufTexture, texCoord);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,111 +4,188 @@
 #include <GLFW/glfw3.h>
 #include "shader_prog.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    glViewport(0, 0, width, height);
+  glViewport(0, 0, width, height);
 }  
 
 void process_input(GLFWwindow *window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);    
-    #endif
+  stbi_set_flip_vertically_on_load(true);
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  #ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  
+  #endif
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "testing", nullptr, nullptr);
-    if (window == nullptr) 
-    {   
-        std::cout << "Failed to create window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+  GLFWwindow* window = glfwCreateWindow(800, 600, "testing", nullptr, nullptr);
+  if (window == nullptr) 
+  {   
+    std::cout << "Failed to create window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
 
-    glfwMakeContextCurrent(window);
+  glfwMakeContextCurrent(window);
+  
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to init GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);  
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  ShaderProg shader_prog(
+  #ifdef __APPLE__  
+    "/Users/Kevin/Development/OpenGL/Testing/src/vertex.glsl",
+    "/Users/Kevin/Development/OpenGL/Testing/src/fragment.glsl"
+  #elif defined(WIN32)
+    "c:\\Users\\liqilong\\Desktop\\OpenGL\\Testing\\src\\vertex.glsl", 
+    "c:\\Users\\liqilong\\Desktop\\OpenGL\\Testing\\src\\fragment.glsl"   
+  #endif
+  );
+
+  float vertices[] = {
+    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left     
+  };
+
+  float tex_coords[] = {
+    0.0f, 0.0f,
+    1.0f, 0.0f,
+    0.5f, 1.0f
+  };
+
+  float border_color[] = {
+    1.0f, 1.0f, 0.0f, 1.0f
+  };
+
+  unsigned int indices[] = {
+    // note that we start from 0!
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+  };
+
+  unsigned int VBO, EBO;
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);  
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  std::string texture_dir = 
+  #ifdef __APPLE__
+    "/Users/Kevin/Development/OpenGL/Testing/textures/";
+  #elif defined(WIN32)
+    "c:\\Users\\liqilong\\Desktop\\OpenGL\\Testing\\src\\vertex.glsl";
+  #endif
+
+  int width, height, n_channels;
+  
+  unsigned int texture, texture2;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  std::string texture_path = texture_dir + "container.jpeg";
+  unsigned char *data = stbi_load(texture_path.c_str(), &width, &height, &n_channels, 0);
+
+  if (data == nullptr) {
+    std::cerr << "cannot load texture: " << texture_path << std::endl;
+  } else {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+
+  stbi_image_free(data);
+  
+  texture_path = texture_dir + "awesomeface.png";
+  data = stbi_load(texture_path.c_str(), &width, &height, &n_channels, 0);
+
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  
+  if (data == nullptr) {
+    std::cerr << "cannot load texture: " << texture_path << std::endl;
+  } else {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }  
+
+  stbi_image_free(data);
+ 
+  int ufColorLocation = glGetUniformLocation(shader_prog.GetProgram(), "ufColor");
+  shader_prog.UseProgram();
+  shader_prog.SetInt("ufTexture", 0);  
+  shader_prog.SetInt("ufTexture2", 1);
+
+  while (!glfwWindowShouldClose(window)) {
+    process_input(window);
     
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to init GLAD" << std::endl;
-        return -1;
-    }
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);  
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    glViewport(0, 0, 800, 600);    
+    shader_prog.UseProgram();
+    float green = (sin(glfwGetTime()) / 2.0f + 0.5f);
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    ShaderProg shader_prog(
-    #ifdef __APPLE__    
-        "/Users/Kevin/Development/OpenGL/Testing/src/vertex.glsl",
-        "/Users/Kevin/Development/OpenGL/Testing/src/fragment.glsl"
-    #elif defined(WIN32)
-        "c:\\Users\\liqilong\\Desktop\\OpenGL\\Testing\\src\\vertex.glsl", 
-        "c:\\Users\\liqilong\\Desktop\\OpenGL\\Testing\\src\\fragment.glsl"     
-    #endif
-    );
-
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // top right
-        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f  // top left 
-    };
-
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };  
-
-    unsigned int VBO, EBO;
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-
+    glUniform4f(ufColorLocation, 0.0f, green, 0.0f, 1.0f);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+     
     glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);    
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    int ufColorLocation = glGetUniformLocation(shader_prog.GetProgram(), "ufColor");
-
-    while (!glfwWindowShouldClose(window)) 
-    {
-        process_input(window);
-        
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);    
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        shader_prog.UseProgram();
-        float green = (sin(glfwGetTime()) / 2.0f + 0.5f);
-
-        glUniform4f(ufColorLocation, 0.0f, green, 0.0f, 1.0f);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwTerminate();
-    return 0;
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+  return 0;
 }
